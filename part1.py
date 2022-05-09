22aa70482f56634a3672ad5e0615525b2591ad5b
Agillian <103528665+Agillian@users.noreply.github.com>
2022-05-08 22:54:36 -0400

    Update part1.py

--- part1.py
+++ part1.py
@@ -22,7 +22,7 @@ if number1 < number2 and number1 < number3:
   smallest = number1
 if number2 < number1 and number2 < number3:
   smallest = number2
-if number3 < number1 and number3 < number3:
+if number3 < number1 and number3 < number2:
   smallest = number3
   
 print("The smallest number is ", smallest)
